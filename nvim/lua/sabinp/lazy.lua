985cbf09b6a3d6d6323e6f0db77243b561dbbb13
Sabin Pocris <dev.sabinpocris@gmail.com>
2023-10-26 15:39:22 +0300

    change theme to gruvbox

--- nvim/lua/sabinp/lazy.lua
+++ nvim/lua/sabinp/lazy.lua
@@ -37,14 +37,26 @@ require("lazy").setup({
   -- },
 
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000 ,
     config = function()
-      vim.cmd([[colorscheme tokyonight-day]])
+      require("gruvbox").setup({
+        contrast = "hard",
+      });
+      vim.o.background = "light"
+      vim.cmd([[colorscheme gruvbox]])
     end
   },
+
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     vim.cmd([[colorscheme tokyonight-day]])
+  --   end
+  -- },
 
   -- Lualine
   {
