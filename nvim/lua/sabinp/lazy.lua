9b102b7fa5719d92e353e8f48d31bc212c616202
Sabin Pocris <dev.sabinpocris@gmail.com>
2023-11-29 12:13:37 +0200

    change theme to onedark

--- nvim/lua/sabinp/lazy.lua
+++ nvim/lua/sabinp/lazy.lua
@@ -30,28 +30,74 @@ require("lazy").setup({
   {
     "ellisonleao/gruvbox.nvim",
     priority = 1000 ,
-    config = function()
-      require("gruvbox").setup({
-        contrast = "hard",
-      });
-      vim.o.background = "dark"
-      vim.cmd([[colorscheme gruvbox]])
+    -- config = function()
+      -- require("gruvbox").setup({
+        -- contrast = "hard",
+      -- });
+      -- vim.o.background = "dark"
+      -- vim.cmd([[colorscheme gruvbox]])
+    -- end
+  },
+
+  {
+    "navarasu/onedark.nvim",
+    config = function ()
+      require('onedark').setup ({
+        -- Main options --
+        style = 'deep', -- Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+        transparent = false,  -- Show/hide background
+        term_colors = true, -- Change terminal color as per the selected theme style
+        ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+        cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+        -- toggle theme style ---
+        toggle_style_key = '<leader>ts', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+        toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+
+        -- Change code style ---
+        -- Options are italic, bold, underline, none
+        -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+        code_style = {
+          comments = 'italic',
+          keywords = 'none',
+          functions = 'none',
+          strings = 'none',
+          variables = 'none'
+        },
+
+        -- Lualine options --
+        lualine = {
+          transparent = false, -- lualine center bar transparency
+        },
+
+        -- Custom Highlights --
+        colors = {}, -- Override default colors
+        highlights = {}, -- Override highlight groups
+
+        -- Plugins Config --
+        diagnostics = {
+          darker = true, -- darker colors for diagnostic
+          undercurl = true,   -- use undercurl instead of underline for diagnostics
+          background = true,    -- use background color for virtual text
+        },
+      })
+      require('onedark').load()
     end
   },
 
-  {
-    'shaunsingh/nord.nvim'
-  },
+  -- {
+  --   'shaunsingh/nord.nvim'
+  -- },
 
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    -- config = function()
-      -- vim.cmd([[colorscheme tokyonight-night]])
-    -- end
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     vim.cmd([[colorscheme tokyonight-night]])
+  --   end
+  -- },
 
   -- Lualine
   {
